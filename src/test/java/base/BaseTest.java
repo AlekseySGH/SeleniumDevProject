097782580edcda77de60f29b6ea2f57023a095d6
AlekseyS <115942572+AlekseySGH@users.noreply.github.com>
2023-01-30 18:01:27 +0200

    some tests added (#3)
    testLogoExistsAndClickableUserAndStartPageRefresh()
    testTopMenuNavigateToCorrespondingPage
    testVerifyAllElementsOfAboutDropdownMenuAreVisible()
    testVerifyAllElementsOfLanguageDropdownMenuAreVisible()
    testVerifyAllElementsOfNavigationBarAreVisible()

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,9 @@
 package base;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.Reporter;
@@ -8,10 +11,12 @@ import org.testng.annotations.*;
 import utils.ReportUtils;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class BaseTest {
 
-    final static String BASE_URL = "https://www.selenium.dev/";
+    private final static String BASE_URL = "https://www.selenium.dev/";
 
     public static String getBaseUrl() {
 
@@ -31,6 +36,69 @@ public abstract class BaseTest {
     public String getCurrentURL() {
 
         return getDriver().getCurrentUrl();
+    }
+
+//    public List<WebElement> getListOfElements(By by) {
+//
+//        return getDriver().findElements(by);
+//    }
+
+    public List<WebElement> getListOfElements(By by) {
+
+        List<WebElement> ListOfElements = new ArrayList<>();
+
+        for (WebElement element : getDriver().findElements(by)) {
+            if (element.isEnabled() && element.isDisplayed()) {
+
+                ListOfElements.add(element);
+            }
+        }
+        return ListOfElements;
+    }
+
+    protected List<String> getListOfElementsText(By by) {
+
+        List<WebElement> list = getListOfElements(by);
+
+        List<String> textList = new ArrayList<>();
+
+        if (list.size() > 0) {
+            for (WebElement element : list) {
+                if (element.isEnabled() && element.isDisplayed()) {
+
+                    textList.add(element.getText());
+                }
+            }
+            return textList;
+        }
+        return new ArrayList<>();
+    }
+
+    public boolean isElementExists(By by) {
+        boolean isExists = true;
+        try {
+            driver.findElement(by);
+        } catch (NoSuchElementException e) {
+            isExists = false;
+        }
+
+        return isExists;
+    }
+
+    public boolean isElementEnabled(By by) {
+
+        return getDriver().findElement(by).isEnabled();
+    }
+
+    public void click(By by) {
+
+        getDriver().findElement(by).click();
+    }
+
+    public void clickForDataProvider(By by, int index) {
+
+        getListOfElements(by).get(index).click();
+
     }
 
     private WebDriver driver;
--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -1,15 +1,38 @@
 package tests;
 
 import base.BaseTest;
+import base.TestData;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class HomePageTest extends BaseTest {
+
+    private final By NAVIGATION_BAR = By.xpath("//div[@id='main_navbar']/ul/li");
+
+    private final By NAVIGATION_BAR_LINK = By.xpath(
+            "//div[@id='main_navbar']/ul/li[@class='nav-item mr-4 mb-2 mb-lg-0']");
+
+    private final By LOGO_IMAGE = By.xpath("//header/nav//span[@class='navbar-logo']");
+
+    private final By ABOUT_DROPDOWNMENU = By.xpath("//div[@class='dropdown-menu show']/a");
+
+    private final By LANGUAGE_DROPDOWNMENU = By.xpath("//div[@class='dropdown-menu show']/a");
+
+    private final By ABOUT_BUTTON = By.xpath("//div//li/a[text() = 'About']");
+
+    private final By LANGUAGE_BUTTON = By.xpath("//div//li/a[text() = 'English']");
+
+    private final By SEARCH_FIELD = By.xpath("//div[@id='docsearch']");
+
 
     @Test
     public void testSeleniumHomePageOpened() {
-        String expectedResultTitle = "Selenium";
-        String expectedResultUrl = "https://www.selenium.dev/";
+        final String expectedResultTitle = "Selenium";
+        final String expectedResultUrl = getBaseUrl();
 
         openBaseURL();
 
@@ -20,4 +43,115 @@ public class HomePageTest extends BaseTest {
         Assert.assertEquals(actualResultUrl, expectedResultUrl);
     }
 
+    @Test
+    public void testVerifyAllElementsOfNavigationBarAreVisible() {
+
+        final List<String> expectedNavigationBarElements = Arrays.asList(
+                "About",
+                "Downloads",
+                "Documentation",
+                "Projects",
+                "Support",
+                "Blog",
+                "English"
+        );
+
+        openBaseURL();
+        List<String> actualNavigationBarElements = getListOfElementsText(NAVIGATION_BAR);
+
+        Assert.assertTrue(isElementExists(LOGO_IMAGE));
+        Assert.assertTrue(isElementExists(SEARCH_FIELD));
+        Assert.assertEquals(actualNavigationBarElements, expectedNavigationBarElements);
+    }
+
+    @Test
+    public void testLogoExistsAndClickableUserAndStartPageRefresh() {
+
+        openBaseURL();
+        click(LOGO_IMAGE);
+
+        Assert.assertTrue(isElementExists(LOGO_IMAGE));
+        Assert.assertTrue(isElementEnabled(LOGO_IMAGE));
+        Assert.assertEquals(getCurrentURL(), getBaseUrl());
+    }
+
+    @Test
+    public void testVerifyAllElementsOfAboutDropdownMenuAreVisible() {
+
+        final List<String> expectedNavigationBarElements = Arrays.asList(
+                "About Selenium",
+                "Structure and Governance",
+                "Events",
+                "Ecosystem",
+                "History",
+                "Get Involved",
+                "Sponsors"
+        );
+
+        openBaseURL();
+        click(ABOUT_BUTTON);
+
+        List<String> actualNavigationBarElements = getListOfElementsText(ABOUT_DROPDOWNMENU);
+
+        Assert.assertEquals(actualNavigationBarElements, expectedNavigationBarElements);
+    }
+
+    @Test
+    public void testVerifyAllElementsOfLanguageDropdownMenuAreVisible() {
+
+        final List<String> expectedNavigationBarElements = Arrays.asList(
+                "Português (Brasileiro)",
+                "中文简体",
+                "日本語",
+                "Other"
+        );
+
+        openBaseURL();
+        click(LANGUAGE_BUTTON);
+
+        List<String> actualNavigationBarElements = getListOfElementsText(LANGUAGE_DROPDOWNMENU);
+
+        Assert.assertEquals(actualNavigationBarElements, expectedNavigationBarElements);
+    }
+
+    @Test(dataProvider = "TopMenuMainPage", dataProviderClass = TestData.class)
+    public void testTopMenuNavigateToCorrespondingPage(
+            int index, String iconName, String href, String expectedURL, String expectedTitle) {
+
+        openBaseURL();
+
+        final String oldTitle = getTitle();
+        final String oldURL = getCurrentURL();
+
+        clickForDataProvider(NAVIGATION_BAR_LINK, index);
+
+        String actualTitle = getTitle();
+        String actualURL = getCurrentURL();
+
+        Assert.assertNotEquals(oldURL, actualURL);
+        Assert.assertNotEquals(oldTitle, actualTitle);
+        Assert.assertEquals(actualURL, expectedURL);
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test(dataProvider = "AboutDropdownMenuMainPage", dataProviderClass = TestData.class)
+    public void aboutDropdownMenuButtonsTestDataProvider(
+            int index, String iconName, String href, String expectedURL, String expectedTitle) {
+
+        openBaseURL();
+
+        final String oldTitle = getTitle();
+        final String oldURL = getCurrentURL();
+
+        click(ABOUT_BUTTON);
+        clickForDataProvider(ABOUT_DROPDOWNMENU, index);
+
+        String actualTitle = getTitle();
+        String actualURL = getCurrentURL();
+
+        Assert.assertNotEquals(oldURL, actualURL);
+        Assert.assertNotEquals(oldTitle, actualTitle);
+        Assert.assertEquals(actualURL, expectedURL);
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
 }
